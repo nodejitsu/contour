4ca15a6957520490e908b6776bc80c19fd084098
3rd-Eden <info@3rd-Eden.com>
2014-05-19 13:30:18 +0200

    [migrate] Migrate to bigpipe 0.6

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -4,7 +4,7 @@
 //
 // Used the proper initialization based on the used analytics tool.
 //
-pipe.once('analytics::initialise', function init(pagelet) {
+pipe.once('analytics:initialize', function init(pagelet) {
   switch (pagelet.data.type) {
     case 'ga':
       ga('create', pagelet.data.key, pagelet.data.domain);
