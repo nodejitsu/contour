1a1e2cfbda6ed46ee49229bc8c44bb1c63f091ca
Swaagie <info@martijnswaagman.nl>
2014-06-03 11:13:47 +0200

    [fix] extend data in example

--- example/bigpipe/pages/index.js
+++ example/bigpipe/pages/index.js
@@ -11,13 +11,15 @@ Page.extend({
   view: './index.hbs',    // The base template we need to render.
   pagelets: {             // The pagelets that should be rendered.
     navigation: contour.navigation.extend({
-      base: '',
-      login: false,
-      signup: true,       // Shows signup button, and relies on recursive pagelets
-      navigation: [
-        { name: 'Cloud', href: '/paas/' },
-        { name: 'Company', href: '/company/' }
-      ]
+      data: {
+        base: '',
+        login: false,
+        signup: true,       // Shows signup button, and relies on recursive pagelets
+        navigation: [
+          { name: 'Cloud', href: '/paas/' },
+          { name: 'Company', href: '/company/' }
+        ]
+      }
     }),
     footer: contour.footer
   }
