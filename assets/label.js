199e9b61af449dec0f4721ddc064ac5db0df044f
Swaagie <info@martijnswaagman.nl>
2014-06-16 11:44:52 +0200

    [minor] add label pagelets

--- assets/label.js
+++ assets/label.js
@@ -1,35 +1,17 @@
 'use strict';
 
 //
-// Expose the alert Pagelet.
+// Expose the label Pagelet.
 //
 module.exports = require('./pagelet').extend({
-  js: [ '{{brand}}/alert/base.js' ],
-  css: [ '{{brand}}/alert/base.styl' ],
-  view: '{{brand}}/alert/view.hbs',
+  css: '{{brand}}/label/base.styl',
+  view: '{{brand}}/label/view.hbs',
 
   //
-  // Default data for alert notificiation, can be changed by using `set`.
+  // Default data for labels, can be changed by using `set`.
   //
-  data: {
-    closable: false,
+  defaults: {
     type: 'notice',
     text: ''
-  },
-
-  //
-  // Used by Square to generate the configuration file. Weight will determine the
-  // relative placement with respect to other assets.
-  //
-  meta: {
-    description: 'Closable alerts in several different colors',
-    weight: 799
-  },
-
-  //
-  // Enlist the client side JS app Alert, which will be added if loader is called.
-  //
-  initialize: function initialize() {
-    if (this.data.closable) this.queue.enlist('loader', { custom: [ 'alert' ] });
   }
 }).on(module);
