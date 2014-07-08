c5953836dc4e9096c793ca07983ad41caa3cb940
Swaagie <info@martijnswaagman.nl>
2014-07-08 11:02:04 +0200

    [fix] improve the formatting behaviour of creditcard pagelet

--- pagelets/nodejitsu/creditcard/base.js
+++ pagelets/nodejitsu/creditcard/base.js
@@ -22,7 +22,9 @@ Cortex.app('Creditcard', Cortex.View.extend({
    */
   delegate: {
     'keyup input[name="full_number"]': 'number',
+    'blur input[name="full_number"]': 'number',
     'keyup input[name="cvv"]': 'cvv',
+    'blur input[name="cvv"]': 'cvv',
     'change select': 'date'
   },
 
@@ -36,14 +38,18 @@ Cortex.app('Creditcard', Cortex.View.extend({
     // [square] @import "creditcard/index.js"
     this.lib = creditcard;
 
+    //
     // Create references to elements.
+    //
     this.number = this.$('input[name="full_number"]');
     this.cvv = this.$('input[name="cvv"]');
     this.year = this.$('select[name="expiration_year"]');
     this.month = this.$('select[name="expiration_month"]');
     this.card = this.$('.card');
 
+    //
     // Is validation required?
+    //
     this.validate = !!this.$el.get('validate');
   },
 
@@ -77,7 +83,9 @@ Cortex.app('Creditcard', Cortex.View.extend({
     var result = this.lib.expiry(this.month.get('value'), this.year.get('value'))
       , className = result ? 'valid' : 'invalid';
 
+    //
     // Update both select boxes.
+    //
     this.month.removeClass('invalid').addClass(className);
     this.year.removeClass('invalid').addClass(className);
   },
@@ -94,30 +102,48 @@ Cortex.app('Creditcard', Cortex.View.extend({
       , key = this.digit(event)
       , valid;
 
+    //
     // Input must be numerical.
-    if (!key.allowed) return;
+    //
+    if (!key.allowed && event.type !== 'blur') return;
 
+    //
+    // Always format if the event is of type blur. This will ensure the input
+    // box does not stay red on blur if formatted value is valid.
+    //
+    if (event.type === 'blur') this.number.set('value', this.lib.format(value));
+
+    //
     // Check if the number is valid.
-    if (value.length >= 16 || key.removal) {
+    //
+    if (value.replace(/\D/g, '').length >= 16 || key.removal) {
       valid = this.lib.validate(value);
 
+      //
       // Show card type or hide if invalid.
+      //
       this.card[valid ? 'removeClass' : 'addClass']('gone');
       this.card.find('strong').set('innerHTML', this.lib.cardscheme(value));
 
       if (this.validate) {
+        //
         // Check against testnumbers in production mode. Valid should be checked
         // as well, otherwise an incomplete credit card number would be valid.
+        //
         if (this.production) {
           valid = valid && !~this.lib.testnumbers.indexOf(+value.replace(/\D/g, ''));
         }
 
+        //
         // Update the styling of the input.
+        //
         this.number.set('className', valid ? 'valid' : 'invalid');
       }
     }
 
+    //
     // Add spaces for each block of 4 characters.
+    //
     this.number.set('value', this.lib.format(value));
   },
 
@@ -132,10 +158,14 @@ Cortex.app('Creditcard', Cortex.View.extend({
       , value = element.value
       , key = this.digit(event);
 
+    //
     // Input must be numerical.
-    if (!key.allowed) return;
+    //
+    if (!key.allowed && event.type !== 'blur') return;
 
+    //
     // Check if the number is valid.
+    //
     if (this.validate && (value.length >= 3 || key.removal)) {
       this.cvv.set(
         'className',
